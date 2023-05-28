fa73845a1a9ebb86c3fba06dd59031e670960bea
nicolepradocruz-ntijohanneberg <nicole.pradocruz@elev.ga.ntig.se>
2023-05-28 20:22:26 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-function toggleMenu() {
-    document.getElementById('navbar2').classList.toggle('nav-active')
-    document.getElementById('burger').classList.toggle('crossed_line')
-}
+// function toggleMenu() {
+//     document.getElementById('navbar2').classList.toggle('nav-active')
+//     document.getElementById('burger').classList.toggle('crossed_line')
+// }
 
-document.getElementById('burger').addEventListener('click', toggleMenu)
+// document.getElementById('burger').addEventListener('click', toggleMenu)
+
+// const burger = document.getElementById('burger')
+// const nav = document.getElementById('navbar2')
+
+// function toggleMenu() {
+//     nav.classList.toggle('nav-active')
+//     burger.classList.toggle('crossed_line')
+// }
+
+// burger.addEventListener('click', toggleMenu)
 
 const burger = document.getElementById('burger')
-const nav = document.getElementById('navbar2')
+const linkList = document.getElementById('link_list')
 
 function toggleMenu() {
-    nav.classList.toggle('nav-active')
-    burger.classList.toggle('crossed_line')
+    linkList.classList.toggle('link_list_visible')
+    burger.classList.toggle('burger_in_x_form')
 }
+
 
 burger.addEventListener('click', toggleMenu)
